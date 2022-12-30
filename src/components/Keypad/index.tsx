52c044602d9d780c02979f5184af506bdfb22647
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2022-12-30 18:16:06 -0300

    CSS Experimentation to better align elements

--- src/components/Keypad/index.tsx
+++ src/components/Keypad/index.tsx
@@ -1,16 +1,49 @@
 import styles from './Keypad.module.scss'
 export default function Keypad(){
     return(
-        <div className={styles.keypad}>
-            <div className={styles.advop}>
-                <p>ADV OPERATIONS</p>
+        <>        
+            <div className={styles.clearBtn}>
+                <button>C</button>
+                <button>AC</button>
             </div>
-            <div className={styles.numbers}>
-                <p>NUMBERS</p>
+
+            <div className={styles.buttons}>
+                <div className={styles.advop}>
+                    <button>X²</button>
+                    <button>RAIZ QDR X</button>
+                    <button>X ^ Y</button>
+                    <button>10 ^ X</button>
+                    <button>LOG</button>
+                    <button>N!</button>
+                    <button>MOD</button>
+                </div>
+                <div className={styles.numbers}>
+                    <button>:</button>
+                    <button>(</button>
+                    <button>)</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+/-</button>
+                    <button>0</button>
+                    <button>,</button>
+
+                </div>
+                <div className={styles.operations}>
+                    <button>/</button>
+                    <button>X</button>
+                    <button>-</button>
+                    <button>+</button>
+                    <button>=</button>
+                </div>
             </div>
-            <div className={styles.operations}>
-                <p>OPERATIONS</p>
-            </div>
-        </div>
+        </>
+
     )
 }
