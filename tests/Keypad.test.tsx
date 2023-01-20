c3501b143d368c68e1f3c39328790fecdfbedf42
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2023-01-20 12:27:12 -0300

    Implementing more tests for Keypad inputs

--- tests/Keypad.test.tsx
+++ tests/Keypad.test.tsx
@@ -8,7 +8,7 @@ describe('The keypad should', () => {
 
     //const currElement = useCurrElement()
 
-    test('input to Recoil Atom State currElement all the number keys from 1 to 0 with a comma(,)', () => {
+    test('input all the number keys from 1 to 0 with a comma(,) and check if it shows into display', () => {
         render(
             <React.StrictMode>
                 <RecoilRoot>
@@ -19,7 +19,7 @@ describe('The keypad should', () => {
 
         const btnToPress = ['1','2','3','4','5','6','7','8','9','0',',','1']
 
-        const displayResult = screen.getByRole('textbox')
+        const displayFormula = screen.getByRole('textbox')
 
         const buttons = []
 
@@ -31,7 +31,88 @@ describe('The keypad should', () => {
             fireEvent.click(button)
         })
   
-        expect(displayResult).toHaveTextContent('1234567890,1')
+        expect(displayFormula).toHaveTextContent('1234567890,1')
+
+    })
+
+    test('input a formula deleting last element and check if the displayFormula is correct with the input', () => {
+        render(
+            <React.StrictMode>
+                <RecoilRoot>
+                    <Kalc />
+                </RecoilRoot>
+            </React.StrictMode>
+        )
+
+        const btnToPress = ['(','-/+','2','X^Y','(','4','5','MOD','2','+','3',')','N!','+',String.fromCharCode(8730),'6','4',')','C']
+
+        const displayFormula = screen.getByRole('textbox')
+
+        const buttons = []
+
+        btnToPress.forEach((item) => {
+            buttons.push(screen.getByText(item, { selector: 'button' }))
+        })
+
+        buttons.forEach((button) => {
+            fireEvent.click(button)
+        })
+  
+        expect(displayFormula).toHaveTextContent(`(-2^(45%2+3)!+${String.fromCharCode(8730)}64`)
+
+    })
+
+    test('input a valid formula with simple operations, calculate and check if the displayResult is correct with the input', () => {
+        render(
+            <React.StrictMode>
+                <RecoilRoot>
+                    <Kalc />
+                </RecoilRoot>
+            </React.StrictMode>
+        )
+
+        const btnToPress = ['(','2','+','(','8','X','(','2','/','2',')',')','-','(','5','X','1','0',')','X','1','0',')','=']
+
+        const displayResult = screen.getByRole('status')
+
+        const buttons = []
+
+        btnToPress.forEach((item) => {
+            buttons.push(screen.getByText(item, { selector: 'button' }))
+        })
+
+        buttons.forEach((button) => {
+            fireEvent.click(button)
+        })
+  
+        expect(displayResult).toHaveTextContent(`-490`)
+
+    })
+
+    test('input a valid formula with advanced operations, calculate and check if the displayResult is correct with the input', () => {
+        render(
+            <React.StrictMode>
+                <RecoilRoot>
+                    <Kalc />
+                </RecoilRoot>
+            </React.StrictMode>
+        )
+
+        const btnToPress = ['2','X^Y','(','4','5','MOD','2','+','3',')','N!','+',String.fromCharCode(8730),'6','4','=']
+
+        const displayResult = screen.getByRole('status')
+
+        const buttons = []
+
+        btnToPress.forEach((item) => {
+            buttons.push(screen.getByText(item, { selector: 'button' }))
+        })
+
+        buttons.forEach((button) => {
+            fireEvent.click(button)
+        })
+  
+        expect(displayResult).toHaveTextContent(`16777224`)
 
     })
 
